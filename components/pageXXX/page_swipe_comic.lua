dfb9c4921208c957fd489958c496a17a8f31947e
Naoya Yamamoto <yamamoto@kwiksher.com>
2016-12-22 16:15:58 +0900

    comic template
    use them with layer_image_set.lua

---
+++ components/pageXXX/page_swipe_comic.lua
@@ -0,0 +1,133 @@
+-- Code created by Kwik - Copyright: kwiksher.com {{year}}
+-- Version: {{vers}}
+-- Project: {{ProjName}}
+--
+local _M = {}
+--
+local _K = require "Application"
+local page_curl  = require("extlib.page_curl")
+local _BackgroundLayerName = "background.jpg"
+--
+{{#ultimate}}
+local bgW, bgH = 1280/4, 1920/4                 --  layer.{{backLayer}}.width, layer.{{backLayer}}.height
+local pgX, pgY = _K.ultimatePosition(640, 960) --  layer.{{backLayer}}.x, layer.{{backLayer}}.y
+local curlWidth = 400/4
+{{/ultimate}}
+{{^ultimate}}
+local bgW, bgH = 1152, 2048
+local pgX, pgY = 768,  1024
+local curlWidth = 400
+{{/ultimate}}
+--
+function _M:allListeners(UI)
+  local sceneGroup  = UI.scene.view
+  local layer       = UI.layer
+  local curPage     = UI.curPage
+  local numPages    = UI.numPages
+  local back, next, prev
+  local prevPage, nextPage
+  nextPage = curPage + 1
+  if nextPage > numPages then nextPage = curPage end
+  prevPage = curPage - 1
+  if prevPage < 1 then prevPage = 1 end
+
+  if layer.{{backLayer}} == nil then return end
+  local W, H = layer.{{backLayer}}.width, layer.{{backLayer}}.height
+  local function Grabbed(event)
+    local curl = event.target
+    if event.dir == "right" then
+      if next == nil and curPage~=nextPage then
+        next = display.newImageRect( _K.imgDir.. "p"..nextPage.."/".._BackgroundLayerName, bgW, bgH )
+        next.x = pgX
+        next.y = pgY
+        sceneGroup:insert(next)
+        next:toFront()
+      end
+    else
+      if prev == nil and curPage ~= prevPage then
+        prev = display.newImageRect( _K.imgDir.."p"..prevPage.."/".._BackgroundLayerName, bgW, bgH )
+        prev.x = pgX
+        prev.y = pgY
+        sceneGroup:insert(prev)
+        prev:toFront()
+      end
+    end
+    back:toFront()
+  end
+  --
+  local function Released(event)
+    back:toBack()
+    if next then
+      next:removeSelf()
+      next = nil
+    end
+    if prev then
+      prev:removeSelf()
+      prev = nil
+    end
+  end
+  --
+  local function Moved (event)
+    local curl, passed_threshold = event.target
+    if event.dir == "right" then
+      passed_threshold = curl.edge_x < .3
+    else
+      passed_threshold = curl.edge_x > .7
+    end
+    if passed_threshold then
+       if event.dir == "right" and _K.kBidi == false then
+          wPage = curPage + 1
+          if wPage > numPages then wPage = curPage end
+          options = { effect = "fromRight"}
+       elseif event.dir == "right" and _K.kBidi == true then
+          wPage = curPage - 1
+          if wPage < 1 then wPage = 1 end
+          options = { effect = "fromLeft"}
+       elseif event.dir == "left" and _K.kBidi == true then
+          wPage = curPage + 1
+          if wPage > numPages then wPage = curPage end
+          options = { effect = "fromRight"}
+       elseif event.dir == "left" and _K.kBidi == false then
+          wPage = curPage - 1
+          if wPage < 1 then wPage = 1 end
+          options = { effect = "fromLeft"}
+       end
+       if tonumber(wPage) ~= tonumber(curPage) then
+            _K.appInstance:showView("views.page0"..wPage.."Scene", options)
+         end
+    end
+  end
+  --
+  if back == nil then
+    back = page_curl.NewPageCurlWidget{width =bgW, height=bgH, size = curlWidth}
+    back:SetImage(_K.imgDir.."p"..curPage.."/".._BackgroundLayerName)
+    back.x = display.contentCenterX - bgW/2
+    back.y  = display.contentCenterY - bgH/2
+    back:SetTouchSides("left_and_right")
+    back:addEventListener("page_grabbed", Grabbed)
+    back:addEventListener("page_dragged", Moved)
+    back:addEventListener("page_released", Released)
+    sceneGroup:insert(back)
+    back:toBack()
+    -- debug mode
+    -- local regions = back:GetGrabRegions()
+    -- for _, region in pairs(regions) do
+    --   local rect = display.newRoundedRect(back.parent, region.x, region.y, region.width, region.height, 12)
+    --   rect:setFillColor(.3, .3)
+    --   rect:setStrokeColor(.4, .5, .2)
+    --   rect.strokeWidth = 10
+    --   sceneGroup:insert(rect)
+    -- end
+  end
+  layer.pageCurl = back
+  layer.pageCurl.alpha = 0
+    layer.{{backLayer}}.alpha = 0
+end
+--
+function _M:dispose()
+end
+--
+function _M:destroy()
+end
+--
+return _M

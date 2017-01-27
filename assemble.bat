036776e324189ca1e49b8181cb23327fd76a1cac
yamamoto <yamamoto@kwiksher.com>
2017-01-27 16:49:11 +0900

    kwik plugin path

--- assemble.bat
+++ assemble.bat
@@ -1,7 +1,7 @@
 @echo GraphicsMagick-X.X.XX-Q8-win32-dll.exe
 @echo ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/windows/
 @echo off
-set PLUGIN_FOLDER={{af}}
+set PLUGIN_FOLDER=C:\Program Files (x86)\Common Files\Adobe\CEP\extensions\com.kwiksher.kwik4\ext\PC
 set PNGASM=apngasm
 set MAGICK=gm convert
 set PROJPATH={{projPath}}

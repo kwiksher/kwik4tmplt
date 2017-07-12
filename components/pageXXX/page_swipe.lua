057a226fca53faedf49b039d9703ce7e201298cb
Yamamoto Naoya <yamamoto@kwiksher.com>
2017-07-12 12:50:05 +0900

    fix for embedded

--- components/pageXXX/page_swipe.lua
+++ components/pageXXX/page_swipe.lua
@@ -17,7 +17,9 @@ local pSpa =  {{pSpa}}/4
 local pSpa  = {{pSpa}}
 {{/ultimate}}
 
+{{#isTmplt}}
 local ui  = require("components.store.UI")
+{{/isTmplt}}
 
 --
 function _M:allListeners(UI)
@@ -32,6 +34,7 @@ function _M:allListeners(UI)
     if layer.{{backLayer}}_2 == nil then return end
     _K.Gesture.activate( layer.{{backLayer}}_2, {swipeLength= pSpa })
   {{/infinity}}
+{{#isTmplt}}
   _K.pageSwap = function (event )
     local options
     if event.phase == "ended" and event.direction ~= nil then
@@ -58,8 +61,49 @@ function _M:allListeners(UI)
             ui.gotoPreviousScene()
           end
        end
+      -- if tonumber(wPage) ~= tonumber(curPage) then
+          {{#hasShake}}
+          Runtime:removeEventListener("accelerometer", _K.shakeMe);
+          {{/hasShake}}
+          {{#invert}}
+          Runtime:removeEventListener("orientation", _K.kOrientation_act);
+          {{/invert}}
+          {{#navigation}}
+            Navigation.hide()
+          {{/navigation}}
+         end
+     -- end
+    end
+{{/isTmplt}}
+{{^isTmplt}}
+  _K.pageSwap = function (event )
+    local options
+    if event.phase == "ended" and event.direction ~= nil then
+       local wPage = curPage
+       if event.direction == "left" and _K.kBidi == false then
+          wPage = curPage + 1
+          if wPage > numPages then wPage = curPage end
+          options = { effect = "fromRight"}
+       elseif event.direction == "left" and _K.kBidi == true then
+          wPage = curPage - 1
+          if wPage < 1 then wPage = 1 end
+          options = { effect = "fromLeft"}
+       elseif event.direction == "right" and _K.kBidi == true then
+          wPage = curPage + 1
+          if wPage > numPages then wPage = curPage end
+          options = { effect = "fromRight"}
+       elseif event.direction == "right" and _K.kBidi == false then
+          wPage = curPage - 1
+          if wPage < 1 then wPage = 1 end
+          options = { effect = "fromLeft"}
+       end
+       if tonumber(wPage) ~= tonumber(curPage) then
+          _K.appInstance:showView("views.page0"..wPage.."Scene", options)
+         end
       end
     end
+{{/isTmplt}}
+
     layer.{{backLayer}}:addEventListener( _K.Gesture.SWIPE_EVENT, _K.pageSwap )
     {{#infinity}}
       layer.{{backLayer}}_2:addEventListener( _K.Gesture.SWIPE_EVENT, _K.pageSwap )
@@ -68,10 +112,12 @@ end
 --
 function _M:toDispose(UI)
     local layer       = UI.layer
-    layer.{{backLayer}}:removeEventListener( _K.Gesture.SWIPE_EVENT, _K.pageSwap )
-    {{#infinity}}
-      layer.{{backLayer}}_2:removeEventListener( _K.Gesture.SWIPE_EVENT, _K.pageSwap )
-    {{/infinity}}
+    if  layer.{{backLayer}} and _K.pageSwap  then
+      layer.{{backLayer}}:removeEventListener( _K.Gesture.SWIPE_EVENT, _K.pageSwap )
+      {{#infinity}}
+        layer.{{backLayer}}_2:removeEventListener( _K.Gesture.SWIPE_EVENT, _K.pageSwap )
+      {{/infinity}}
+   end
   --_K.Gesture.deactivate(layer.{{myLName+') ;
 end
 --

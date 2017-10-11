dd91aefd14109baef9028788ece50ece3498cfd4
Yamamoto Naoya <yamamoto@kwiksher.com>
2017-10-11 20:04:55 +0900

    fsm init

--- components/store/UI.lua
+++ components/store/UI.lua
@@ -29,7 +29,7 @@ local function getPageNum(num)
 end
 M.getPageNum = getPageNum
 --
-local function readPageJson(epsode)
+local function readPageJson(name)
     local jsonFile = function(filename )
        local path = system.pathForFile(filename, system.ApplicationSupportDirectory)
        --print(path)
@@ -43,13 +43,15 @@ local function readPageJson(epsode)
        end
        return contents
     end
-    currentBook = epsode
-    currentBookModel =  json.decode( jsonFile(epsode.."/model.json") )
+    currentBook = name
+    currentBookModel =  json.decode( jsonFile(name.."/model.json") )
     -- for k, v in pairs(currentBookModel) do
     --     for l, m in pairs(v) do print(l, m) end
     -- end
     M.numPages = #currentBookModel
 end
+--
+M.readPageJson = readPageJson
 --
 _K.getModel = function(layerName, imagePath, dummy)
     local targetPage = dummy or M.currentPage
@@ -104,19 +106,11 @@ setSystemDir[type.tmplt] = function (isDL, pageNum)
         _K.systemDir = system.ApplicationSupportDirectory
         _K.imgDir = currentBook.."/images/"
         _K.audioDir = currentBook.."/audios/p"..targetPage.."_"
-        _K.spriteDir   = currentBook.."/sprites/"
-        _K.thumbDir    = currentBook.."/thumbnails/"
-        _K.videoDir    = currentBook.."/videos/"
-        _K.particleDir = currentBook.."/particles/"
     else
         _K.systemDir = system.ResourceDirectory
-        _K.imgDir      = "assets/images/"
-        _K.audioDir    = "assets/audios/"
-        _K.spriteDir   = "assets/sprites/"
-        _K.thumbDir    = "assets/thumbnails/"
-        _K.videoDir    = "assets/videos/"
-        _K.particleDir = "assets/particles/"
- end
+        _K.imgDir = "assets/images/"
+        _K.audioDir = "assets/audios/"
+    end
 end
 --
 M.setDir = function(pageNum)
@@ -133,10 +127,6 @@ M.setDir = function(pageNum)
                 _K.systemDir = system.ResourceDirectory
                 _K.imgDir = "assets/images/"
                 _K.audioDir = "assets/audios/"
-                _K.spriteDir   = "assets/sprites/"
-                _K.thumbDir    = "assets/thumbnails/"
-                _K.videoDir    = "assets/videos/"
-                _K.particleDir = "assets/particles/"
                 composer.gotoScene("views.page0"..model.epsodes[epname].info:sub(2).."Scene")
             else
                 local infoString = "The page is not found."
@@ -182,7 +172,7 @@ M.gotoNextScene = function(params)
     local prevAlias = getPageNum(M.currentPage)
     local nextAlias = getPageNum(M.currentPage+1)
     M.currentPage = M.currentPage + 1
-    if prevAlias == nextAlias then
+    if prevAlias == nextAlias  then
         --setSystemDir[type.tmplt](false)
         composer.gotoScene("extlib.page_reload")
     else
@@ -358,282 +348,9 @@ M.replaceTimeCodes = function(syncLayer, filename)
         end
     end
 end
---
-function M.new()
-    local UI = {}
-    --
-    UI.downloadGroup = {}
-    UI.sceneGroup    = nil
-    --
-    function UI.gotoScene(event)
-        local epsode =  event.target.selectedPurchase
-        print("UI.gotoScene ".. epsode)
-        if master.isEmbedded then
-            print("  isEmbedded")
-            Runtime:dispatchEvent({name="changeThisMug", appName=epsode})
-        elseif bookShelfType == type.tmplt then
-            print("  tmplt")
-            readPageJson(epsode)
-            setSystemDir[type.tmplt](true)
-            M.currentPage = 1
-            print("  views.page0"..getPageNum(1).."Scene")
-            composer.gotoScene("views.page0"..getPageNum(1).."Scene")
-        else
-            print("  pages")
-            setSystemDir[type.pages](epsode)
-            composer.gotoScene(model.getPageName(epsode) , {effect=model.gotoSceneEffect})
-        end
-        return true
-    end
-    --
-    function UI.showOverlay(event)
-        if event.target.purchaseBtn.alpha ==  0 then return true end
-        local epsode =  event.target.selectedPurchase
-        local options = {
-            isModal = true,
-            effect = model.showOverlayEffect,
-            time = 400,
-            params = {}
-        }
-        local page = "views.page02Scene" -- INFO
-        if  bookShelfType == type.tmplt then
-             page = model.getPageInfo(epsode)
-        end
-        if page then
-            if master.isEmbedded then
-                package.loaded[page] = require("plugin.KwikShelf."..page)
-            end
-            model.currentEpsode = {name=epsode}
-            composer.showOverlay(page, options)
-        end
-        return true
-    end
-    --
-    local function copyDisplayObject(src, dst)
-        local obj = display.newImageRect( _K.imgDir..src.imagePath, _K.systemDir, src.width, src.height)
-            obj.x = dst.x + src.x - _W/2
-            obj.y = dst.y + src.y - _H/2
-            src.alpha = 0
-        return obj
-    end
-    --
-    function UI:addEventListener(button, epsode)
-        if (IAP.getInventoryValue("unlock_"..epsode.name)==true) then
-            if downloadManager.hasDownloaded(epsode.name) then
-                print(epsode.name .."(saved)")
-                button.savedBtn:addEventListener("tap", self.gotoScene)
-                button:addEventListener("tap", self.gotoScene)
-                if model.URL then
-                    button.savingTxt.alpha = 0
-                end
-                button.savedBtn.alpha = 1
-            else
-                print(epsode.name.."(saving)")
-                button.savingTxt.alpha = 1
-                Runtime:dispatchEvent({name = "downloadManager:purchaseCompleted", target = epsode.name})
-            end
-        else
-            print(epsode.name.."(not purchased)")
-            --Otherwise add a tap listener to the button that unlocks the epsode
-            button.purchaseBtn.alpha = 1
-            button.purchaseBtn:addEventListener("tap", function(e)
-                print("puchaseBtn", self)
-                IAP.buyEpsode(e)
-                return true
-                end)
-            if self.overlay then
-                button:addEventListener("tap", self.showOverlay)
-            end
-        end
-    end
-
-    --
-    function UI:create(_epsode)
-        local layer = self.layer
-        local overlay = self.overlay
-        print("--- ui create ---")
-        function setButton(layer, button, epsode)
-            button.selectedPurchase = epsode.name
-            --If the user has purchased the epsode before, change the button
-            local purchaseBtn = copyDisplayObject(layer.purchaseBtn, button)
-            purchaseBtn.alpha = 0
-            purchaseBtn.selectedPurchase = epsode.name
-            self.sceneGroup:insert(purchaseBtn)
-            button.purchaseBtn = purchaseBtn
-            print(purchaseBtn)
-            if model.URL then
-                local downloadBtn = copyDisplayObject(layer.downloadBtn, button)
-                downloadBtn.alpha = 0
-                downloadBtn.selectedPurchase = epsode.name
-                self.sceneGroup:insert(downloadBtn)
-                button.downloadBtn = downloadBtn
-
-                local savingTxt = copyDisplayObject(layer.savingTxt, button)
-                savingTxt.alpha = 0
-                self.sceneGroup:insert(savingTxt)
-                button.savingTxt = savingTxt
-            end
-            local savedBtn = copyDisplayObject(layer.savedBtn, button)
-            savedBtn.alpha = 0
-            savedBtn.selectedPurchase = epsode.name
-            self.sceneGroup:insert(savedBtn)
-            button.savedBtn = savedBtn
-
-            self:addEventListener(button, epsode)
-            --
-            print("---- SetButton --- ", self, button)
-            self.downloadGroup[epsode.name] = button
-            --
-            -- button image
-            --
-            downloadManager.setButtonImage(button, epsode.name)
-        end
-        if _epsode then -- infoPage
-            if layer["bookXXIcon"] then
-                setButton(layer, layer["bookXXIcon"], _epsode)
-                if layer.hideOverlayBtn then
-                    layer.hideOverlayBtn:addEventListener("tap", function ()
-                        composer.hideOverlay("fade", 400 )
-                        end)
-                end
-                if layer.infoTxt then
-                    layer.infoTxt.text = model.epsodes[_epsode.name].info
-                end
-            end
-        else -- TOC
-            for k, epsode in pairs( model.epsodes) do
-                -- print(epsode.name)
-                local button = layer[epsode.name.."Icon"]
-                if button then
-                    setButton(layer, button, epsode)
-                end
-            end
-        end
-        --
-        if layer.restoreBtn then
-            layer.restoreBtn:addEventListener("tap",
-                function(event)
-                    for k, epsode in pairs (model.epsodes) do
-                        local button = layer[epsode.name.."Icon"]
-                        button:removeEventListener("tap", self.gotoScene)
-                        button.savedBtn:removeEventListener("tap", self.gotoScene)
-                    end
-                    IAP.restorePurchases(event)
-                end)
-        end
-    end
-    --
-    function UI:destroy()
-        for k, epsode in pairs( model.epsodes) do
-            local button = self.layer[epsode.name.."Icon"]
-            if button then
-                button.purchaseBtn:removeEventListener("tap", IAP.buyEpsode)
-                button:removeEventListener("tap", self.showOverlay)
-                button.savedBtn:removeEventListener("tap", self.gotoScene)
-                button:removeEventListener("tap", self.gotoScene)
-             end
-        end
-
-    end
-    --
-    function UI:refresh()
-        UI.cmd:init(self)
-        for k, epsode in pairs( model.epsodes) do
-            local button = self.layer[epsode.name.."Icon"]
-            if button then
-                print("-------- refresh ---------", self,  button)
-                self.downloadGroup[epsode.name] = button
-                button.purchaseBtn.alpha      = 0
-                if model.URL then
-                    button.downloadBtn.alpha      = 0
-                    button.savingTxt.alpha        = 0
-                    button.savedBtn.alpha         = 0
-                end
-                button.purchaseBtn:removeEventListener("tap", IAP.buyEpsode)
-                button:removeEventListener("tap", self.showOverlay)
-                button.savedBtn:removeEventListener("tap", self.gotoScene)
-                button:removeEventListener("tap", self.gotoScene)
-
-                self:addEventListener(button, epsode)
-                --
-             end
-        end
-    end
---[[
-    --
-    function UI:createBuyButton(epsode, x, y, width, height)
-        --Create button background
-        local buyBackground = display.newRect(0, 0, width, height)
-        buyBackground.stroke = { 0.5, 0.5, 0.5 }
-        buyBackground.strokeWidth = 2
-        --Create "buy IAP" text object
-        local buyText = display.newText(epsode, 0, 0, native.systemFont, 18)
-        buyText:setFillColor(0,0,0)
-        --Place objects into a group
-        local button            = display.newGroup()
-        button.text             = buyText
-        button.selectedPurchase = epsode
-        button.x = x
-        button.y = y
-        button:insert(buyBackground)
-        button:insert(buyText)
-
-        --If the user has purchased the epsode before, change the button
-        if (IAP.getInventoryValue("unlock_"..epsode)==true) then
-            if downloadManager.hasDownloaded(epsode) then
-                buyText.text=epsode .."(saved)"
-                button:addEventListener("tap", self.gotoScene)
-            else
-                buyText.text="saving"
-                Runtime:dispatchEvent({name = "downloadManager:purchaseCompleted", target = epsode})
-            end
-        else
-            --Otherwise add a tap listener to the button that unlocks the epsode
-            button:addEventListener("tap", IAP.buyEpsode)
-        end
-        --
-         self.downloadGroup[epsode] = button
-         self.sceneGroup:insert(button)
-    end
-
-    function UI:createRestoreButton()
-            --Draw "restore" button
-        --Create button background
-        local restoreBackground = display.newRect(_W/2, _H/2+100, 180, 50)
-        restoreBackground.stroke = { 0.5, 0.5, 0.5 }
-        restoreBackground.strokeWidth = 2
-        --Create "buy IAP" text object
-        local restoreText = display.newText("Restore purchases", restoreBackground.x, restoreBackground.y, native.systemFont, 18)
-        restoreText:setFillColor(0,0,0)
-        --Add event listener
-        restoreText:addEventListener("tap",
-            function(event)
-                self.downloadGroup[model.epsode02]:removeEventListener("tap", self.gotoScene)
-                self.downloadGroup[model.epsode03]:removeEventListener("tap", self.gotoScene)
-                IAP.restorePurchases(event)
-            end)
-        local group = display.newGroup()
-        group:insert(restoreBackground)
-        group:insert(restoreText)
-        self.sceneGroup:insert(group)
-    end
---]]
-
-    function UI:init(group, layer, overlay)
-        self.sceneGroup = group
-        self.layer      = layer
-        self.overlay    = overlay
-    end
-    --
-    return UI
-end
-
-function M.init(url)
-    model.URL = url
-    IAP:init(model.catalogue, nil, nil)
-end
 
 function M.hasDownloaded(epsode)
+    IAP:init(model.catalogue, nil, nil)
     if (IAP.getInventoryValue("unlock_"..epsode)==true) then
         if downloadManager.hasDownloaded(epsode) then
             return true
@@ -651,6 +368,26 @@ function M.isPageReady(num)
         if epsode then
             return M.hasDownloaded(epsode)
         end
+    end
+    return true
+end
+
+function M.gotoScene(event)
+    local epsode =  event.target.selectedPurchase
+    print("UI.gotoScene ".. epsode.name)
+    if master.isEmbedded then
+        Runtime:dispatchEvent({name="changeThisMug", appName=epsode.name})
+    elseif bookShelfType == type.tmplt then
+        readPageJson(epsode.name)
+        print("#############")
+        setSystemDir[type.tmplt](true)
+        print("#############")
+        M.currentPage = 1
+        print("views.page0"..getPageNum(1).."Scene")
+        composer.gotoScene("views.page0"..getPageNum(1).."Scene")
+    else
+        setSystemDir[type.pages](epsode.name)
+        composer.gotoScene(model.getPageName(epsode.name) , {effect=model.gotoSceneEffect})
     end
     return true
 end

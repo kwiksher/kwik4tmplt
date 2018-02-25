158e1dbdf3926e405bc1358586b014506a2b9a9b
Naoya Yamamoto <yamamoto@kwiksher.com>
2018-02-25 12:10:38 +0900

    FIX: suspend with Bookshef

--- commands/app/suspend.lua
+++ commands/app/suspend.lua
@@ -4,7 +4,13 @@
 --
 local _Command = {}
 local composer = require("composer")
-local storeFSM = require("components.store.storeFSM").getInstance()
+local storeFSM = nil
+local model    = nil
+local path = system.pathForFile("components/store/storeFSM.lua", system.ResourceDirectory )
+if path then
+   storeFSM = require("components.store.storeFSM").getInstance()
+   model       = require("components.store.model")
+end
 -----------------------------
 -----------------------------
 function _Command:new()
@@ -20,15 +26,18 @@ function _Command:new()
 			          if (system.getInfo( "platform" ) == "Android")  then
 			              native.requestExit()
 			          else
-			          		if storeFSM.fsm:getState().name == "ThumbnailDisplayed" then
+			          		if storeFSM == nil or storeFSM.fsm:getState().name == "ThumbnailDisplayed" then
 					              if nil~= composer.getScene("views.page01Scene") then
 		                      print("=== suspend remove page01Scene === ")
 					                composer.removeScene("views.page01Scene", true)
 					              end
 				            end
 			              print("==== suspend =====")
-			              storeFSM.fsm:showThumbnail()
-			              -- composer.gotoScene("views.page01Scene")
+			              if storeFSM and (model.bookShelfType == 1 or model.bookShelfType == 2) then
+		    	              storeFSM.fsm:showThumbnail()
+				            else
+    			              composer.gotoScene("views.page01Scene")
+				            end
 			          end
 			       end
 			    end

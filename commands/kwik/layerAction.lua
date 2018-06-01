1ebe6917b52e3f7cf1315e0efee45e40f0f239b8
Naoya Yamamoto <yamamoto@kwiksher.com>
2018-06-01 17:10:24 +0900

    Modify:ShowHide with delay 0 and time 0

--- commands/kwik/layerAction.lua
+++ commands/kwik/layerAction.lua
@@ -6,18 +6,40 @@ local _M = {}
 --		obj:showHide("objB", false)
 function _M:showHide(obj, hides, toggles, time, delay)
   local timer
-   if toggles then
-        if obj.alpha == 0 then
-            timer = transition.to( obj, {alpha=obj.oldAlpha, time=time, delay=delay})
-        else
-            timer = transition.to( obj, {alpha=0, time=time, delay=delay})
-        end
-    else
-        if hides then
+   if delay > 0 or time > 0 then
+     if toggles then
+          if obj.alpha == 0 then
               timer = transition.to( obj, {alpha=obj.oldAlpha, time=time, delay=delay})
-        else
+          else
               timer = transition.to( obj, {alpha=0, time=time, delay=delay})
-        end
+          end
+      else
+          if hides then
+            obj.oldAlpha = 1
+                timer = transition.to( obj, {alpha=obj.oldAlpha, time=time, delay=delay})
+                print("obj.oldAlpha", obj.oldAlpha)
+                print("hides", obj.alpha)
+          else
+                timer = transition.to( obj, {alpha=0, time=time, delay=delay})
+                print("not hides", obj.alpha)
+          end
+    end
+  else
+     if toggles then
+          if obj.alpha == 0 then
+              obj.alpha = obj.oldAlpha
+          else
+              obj.alpha = 0
+          end
+      else
+          if hides then
+            obj.oldAlpha = 1
+            obj.alpha = 1
+         else
+              obj.alpha = 0
+         end
+    end
+
   end
   return timer
 end

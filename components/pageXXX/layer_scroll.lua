c9dde3927e0c8801b144fff1ff35f0ab343af9d2
Naoya Yamamoto <yamamoto@kwiksher.com>
2020-05-14 13:29:56 +0900

    Update layer_scroll.lua

--- components/pageXXX/layer_scroll.lua
+++ components/pageXXX/layer_scroll.lua
@@ -15,31 +15,38 @@ end
 --
 {{#ultimate}}
   {{#para}}
-  local ggwid  = {{ggwid}}/4
+  local widthGroupMember  = {{ggwid}}/4
   {{/para}}
   {{#page}}
   local gww, gwh   = {{gww}}/4, {{gwh}}/4
   local gwsw, gwsh = {{gwsw}}/4, {{gwsh}}/4
   {{/page}}
   {{#object}}
-  local ggwid  = {{ggwid}}/4
+  local widthGroupMember = {{ggwid}}/4
   {{/object}}
   {{#manual}}
-  local gmt, gml   = {{gmt}}, {{gml}}
-  local gww, gwh   = {{gww}}/4, {{gwh}}/4
-  local gwsw, gwsh = {{gwsw}}/4, {{gwsh}}/4
+    {{#b1x}}
+    local gmt, gml   = {{gmt}}, {{gml}}
+    local gww, gwh   = {{gww}}, {{gwh}}
+    local gwsw, gwsh = {{gwsw}}, {{gwsh}}
+    {{/b1x}}
+    {{^b1x}}
+    local gmt, gml   = {{gmt}}, {{gml}}
+    local gww, gwh   = {{gww}}/4, {{gwh}}/4
+    local gwsw, gwsh = {{gwsw}}/4, {{gwsh}}/4
+    {{/b1x}}
   {{/manual}}
 {{/ultimate}}
 {{^ultimate}}
   {{#para}}
-  local ggwid  = {{ggwid}}
+  local widthGroupMember  = {{ggwid}}
   {{/para}}
   {{#page}}
   local gww, gwh   = {{gww}}, {{gwh}}
   local gwsw, gwsh = {{gwsw}}, {{gwsh}}
   {{/page}}
   {{#object}}
-  local ggwid  = {{ggwid}}
+  local widthGroupMember  = {{ggwid}}
   {{/object}}
   {{#manual}}
   local gmt, gml   = {{gmt}}, {{gml}}
@@ -52,46 +59,52 @@ function _M:didShow(UI)
   local sceneGroup  = UI.scene.view
   local layer       = UI.layer
   if layer.{{glayer}} == nil then return end
-
+    local _top          = 0
+    local _left         = 0
+    local _width        = 0
+    local _height       = 0
+    local _scrollWidth  = 0
+    local _scrollHeight = 0
+--
   {{^layer}}
    {{#para}}
-    local _top         = layer.{{glayer}}.contentBounds.yMin
-    local _left        = layer.{{glayer}}.contentBounds.xMin
-    local _width       = layer.{{glayer}}.width + 10 - ggwid
-    local _height      = layer.{{glayer}}.height
-    local _scrollWidth = layer.{{glayer}}.width
-    local _scrollHeight = layer.{{glayer}}.height
+    _top         = layer.{{glayer}}.contentBounds.yMin
+    _left        = layer.{{glayer}}.contentBounds.xMin
+    _width       = layer.{{glayer}}.width + 10 - widthGroupMember
+    _height      = layer.{{glayer}}.height
+    _scrollWidth = layer.{{glayer}}.width
+    _scrollHeight = layer.{{glayer}}.height
   {{/para}}
   {{#page}}
-    local _top          = layer.{{glayer}}.contentBounds.yMin
-    local _left         = layer.{{glayer}}.contentBounds.xMin
-    local _width        = gww
-    local _height       = gwh
-    local _scrollWidth  = gwsw
-    local _scrollHeight = gwsh
+    _top          = layer.{{glayer}}.contentBounds.yMin
+    _left         = layer.{{glayer}}.contentBounds.xMin
+    _width        = gww or 0
+    _height       = gwh or 0
+    _scrollWidth  = gwsw or 0
+    _scrollHeight = gwsh or 0
   {{/page}}
   {{#object}}
-    local _top          = layer.{{glayer}}.contentBounds.yMin
-    local _left         = layer.{{glayer}}.contentBounds.xMin
-    local _width        = layer.{{glayer}}.width + 10- ggwid
-    local _height       = layer.{{glayer}}.height
-    local _scrollWidth  = layer.{{glayer}}.width
-    local _scrollHeight = layer.{{glayer}}.height
+    _top          = layer.{{glayer}}.contentBounds.yMin
+    _left         = layer.{{glayer}}.contentBounds.xMin
+    _width        = layer.{{glayer}}.width + 10- widthGroupMember
+    _height       = layer.{{glayer}}.height
+    _scrollWidth  = layer.{{glayer}}.width
+    _scrollHeight = layer.{{glayer}}.height
   {{/object}}
   {{#manual}}
-    local _top          = gmt
-    local _left         = gml
-    local _width        = gww
-    local _height       = gwh
-    local _scrollWidth  = gwsw
-    local _scrollHeight = gwsh
+    _top          = gmt or _top
+    _left         = gml or _left
+    _width        = gww or _width
+    _height       = gwh or _height
+    _scrollWidth  = gwsw or _scrollWidth
+    _scrollHeight = gwsh or _scrollHeight
   {{/manual}}
-
+--
   _width        = (_width        ==0) and layer.{{glayer}}.width or _width
   _height       = (_height       ==0) and layer.{{glayer}}.height or _height
   _scrollWidth  = (_scrollWidth  ==0) and layer.{{glayer}}.width or _scrollWidth
   _scrollHeight = (_scrollHeight ==0) and layer.{{glayer}}.height or _scrollHeight
-
+--
   layer.{{gname}} = widget.newScrollView( {
     top          = _top,
     left         = _left,
@@ -118,48 +131,62 @@ function _M:didShow(UI)
   })
   sceneGroup:insert( layer.{{gname}})
   layer.{{gname}}:insert(layer.{{glayer}})
-  layer.{{glayer}}.x = layer.{{glayer}}.width / 2
-  layer.{{glayer}}.y = layer.{{glayer}}.height / 2
+
+  {{^manual}}
+    layer.{{glayer}}.x = layer.{{glayer}}.width / 2
+    layer.{{glayer}}.y = layer.{{glayer}}.height / 2
+  {{/manual}}
+  {{#manual}}
+    {{#gdH}}
+      layer.{{glayer}}.x = layer.{{glayer}}.width / 2
+      --layer.{{glayer}}.y = layer.{{glayer}}.height / 2
+    {{/gdH}}
+    {{#gdV}}
+      -- layer.{{glayer}}.x = layer.{{glayer}}.width / 2
+      layer.{{glayer}}.y = layer.{{glayer}}.height / 2
+  {{/gdV}}
+  {{/manual}}
 {{/layer}}
+
 {{#layer}}
   {{#para}}
-    local _top    = layer.{{glayer}}.y
-    local _left   = layer.{{glayer}}.x
-    local _width  = layer.{{glayer}}.originalW
-    local _height = layer.{{glayer}}.originalH
-    local _scrollWidth  = gwsw
-    local _scrollHeight = gwsh
+    _top    = layer.{{glayer}}.y
+    _left   = layer.{{glayer}}.x
+    _width  = layer.{{glayer}}.originalW
+    _height = layer.{{glayer}}.originalH
+    _scrollWidth  = gwsw
+    _scrollHeight = gwsh
   {{/para}}
   {{#page}}
-    local _top    = layer.{{glayer}}.y
-    local _left   = layer.{{glayer}}.x
-    local _width  = layer.{{glayer}}.width + 10
-    local _height = gwh
-    local _scrollWidth  = gwsw
-    local _scrollHeight = gwsh
+    _top    = layer.{{glayer}}.y
+    _left   = layer.{{glayer}}.x
+    _width  = layer.{{glayer}}.width + 10
+    _height = gwh
+    _scrollWidth  = gwsw
+    _scrollHeight = gwsh
   {{/page}}
   {{#object}}
-    local _top    = layer.{{glayer}}.y
-    local _left   = layer.{{glayer}}.x
-    local _width  = layer.{{glayer}}.width + 10
-    local _height = layer.{{glayer}}.height
-    local _scrollWidth  = gwsw
-    local _scrollHeight = gwsh
+    _top    = layer.{{glayer}}.y
+    _left   = layer.{{glayer}}.x
+    _width  = layer.{{glayer}}.width + 10
+    _height = layer.{{glayer}}.height
+    _scrollWidth  = gwsw
+    _scrollHeight = gwsh
   {{/object}}
   {{#manual}}
-    local _top          = gmt
-    local _left         = gml
-    local _width        = gww
-    local _height       = gwh
-    local _scrollWidth  = gwsw
-    local _scrollHeight = gwsh
+    _top          = gmt
+    _left         = gml
+    _width        = gww
+    _height       = gwh
+    _scrollWidth  = gwsw
+    _scrollHeight = gwsh
   {{/manual}}
-
-  _width        = (_width        ==0) and layer.{{glayer}}.height or _width
+--
+  _width        = (_width        ==0) and layer.{{glayer}}.width or _width
   _height       = (_height       ==0) and layer.{{glayer}}.height or _height
-  _scrollWidth  = (_scrollWidth  ==0) and layer.{{glayer}}.height or _scrollWidth
+  _scrollWidth  = (_scrollWidth  ==0) and layer.{{glayer}}.width or _scrollWidth
   _scrollHeight = (_scrollHeight ==0) and layer.{{glayer}}.height or _scrollHeight
-
+--
   layer.{{gname}} = widget.newScrollView ({
     top          = _top,
     left         = _left,

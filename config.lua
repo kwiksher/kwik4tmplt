228245898826210fda95d23ed8d83a7cf50b242d
Yamamoto Naoya <yamamoto@kwiksher.com>
2020-09-22 17:30:43 +0900

    config.lua for Ultimate enabled kScale

--- config.lua
+++ config.lua
@@ -3,13 +3,14 @@
 -- Project: {{ProjName}}
 --
 {{#ultimate}}
+local kScale = "{{kk}}"
 --calculate the aspect ratio of the device:
 local aspectRatio = display.pixelHeight / display.pixelWidth
 application = {
    content = {
       width = aspectRatio > 1.5 and 320 or math.ceil( 480 / aspectRatio ),
       height = aspectRatio < 1.5 and 480 or math.ceil( 320 * aspectRatio ),
-      scale = "letterBox",
+      scale = kScale,
       fps = 30,
       imageSuffix = {
          ["@2x"] = 1.5,
